cd653609a67974b2bd96b478dfe8dd1e0d292b81
Lea Schmoll <s52113@edu.campus02.at>
2022-12-03 17:24:04 +0100

    Finished part 2 of Day1.

--- day1.py
+++ day1.py
@@ -5,16 +5,14 @@ def countCalories(elvesInventory : list) -> str:
     elfCounter = 0
     elfRanking.append(0)
     for indx in range(len(elvesInventory)):
-        if elvesInventory[indx][0] == " ":
+        if elvesInventory[indx] == " ":
             elfCounter += 1
             elfRanking.append(0)
         else:
-            elfRanking[elfCounter] += int(elvesInventory[indx][0])
+            elfRanking[elfCounter] += int(elvesInventory[indx])
     return elfRanking
 
 inventoryTest = [1000, 2000, 3000,"", 4000, "", 5000, 6000, "", 7000, 8000, 9000, "",10000]
-
-# inputDay1 = open("InputDay1.txt", "r")
 
 inventory = []
 
@@ -26,9 +24,21 @@ with open('InputDay1.csv') as f:
         if zeile == []:
             inventory.append(default)
         else:
-            inventory.append(zeile)
+            inventory.append(zeile[0])
 
 print(type(inventory))
 
+elfCalories = countCalories(inventory)
 
-print(countCalories(inventory))
+print(f"The top elf carries {max(elfCalories)} calories in total.")
+
+maxthree = 0
+
+for x in range(1, 4):
+    maxthree = maxthree + sorted(elfCalories)[-x]
+
+# print(a)
+
+# maxthree = sorted(elfCalories)[-1] + sorted(elfCalories)[-2] +sorted(elfCalories)[-3]
+
+print(f"The top three Elves carry {maxthree} calories in total.")
